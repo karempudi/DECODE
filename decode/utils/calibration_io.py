c410b1ce35ae52c7c1de405af1105aa74cf30716
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2022-12-22 15:11:40 +0100

    Plugged in cspline model from python pickle format

--- decode/utils/calibration_io.py
+++ decode/utils/calibration_io.py
@@ -1,5 +1,6 @@
 import scipy.io as sio
 import torch
+import pickle
 
 import decode.simulation.psf_kernel as psf_kernel
 
@@ -18,6 +19,41 @@ class SMAPSplineCoefficient:
         self.coeff = torch.from_numpy(self.calib_mat.cspline.coeff)
         self.ref0 = (self.calib_mat.cspline.x0 - 1, self.calib_mat.cspline.x0 - 1, self.calib_mat.cspline.z0)
         self.dz = self.calib_mat.cspline.dz
+        self.spline_roi_shape = self.coeff.shape[:3]
+
+    def init_spline(self, xextent, yextent, img_shape, device='cuda:0' if torch.cuda.is_available() else 'cpu', **kwargs):
+        """
+        Initializes the CubicSpline function
+
+        Args:
+            xextent:
+            yextent:
+            img_shape:
+            device: on which device to simulate
+
+        Returns:
+
+        """
+        psf = psf_kernel.CubicSplinePSF(xextent=xextent, yextent=yextent, img_shape=img_shape, ref0=self.ref0,
+                                        coeff=self.coeff, vx_size=(1., 1., self.dz), device=device, **kwargs)
+
+        return psf
+
+class SplineCoefficient:
+    """Wrapper class as an interface for Python Spline calibration data."""
+    def __init__(self, calib_file):
+        """
+        Loads a calibration file from SMAP and the relevant meta information
+        Args:
+            file:
+        """
+        self.calib_file = calib_file
+        with open(self.calib_file, 'rb') as fh:
+            self.calib_dict = pickle.load(fh)
+
+        self.coeff = torch.from_numpy(self.calib_dict['coeff'])
+        self.ref0 = (self.calib_dict['x0'], self.calib_dict['x0'], self.calib_dict['z0'])
+        self.dz = self.calib_dict['dz']
         self.spline_roi_shape = self.coeff.shape[:3]
 
     def init_spline(self, xextent, yextent, img_shape, device='cuda:0' if torch.cuda.is_available() else 'cpu', **kwargs):
